8ab5eb886f4190a244402b97eb7684b0b974785e
jesus <jesusrubio3>
2021-11-30 12:13:13 +0100

    ficheros

---
+++ appmezzanine/local_settings.py
@@ -0,0 +1,28 @@
+# This file is exec'd from settings.py, so it has access to and can
+# modify all the variables in settings.py.
+
+DEBUG = True
+
+# Make these unique, and don't share it with anybody.
+SECRET_KEY = "django-insecure-1-rz(t9l#k$q&$*c-)u@b4=yzvywu%m+kawth7_*jd+09sgw8w"
+NEVERCACHE_KEY = "=x#&az(p&-0@7f%(7w*$tjj(=q+r@)h(pjbh74sn@@as(6cp$b"
+
+DATABASES = {
+    "default": {
+        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.sqlite3",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "dev.db",
+        # Not used with sqlite3.
+        "USER": "",
+        # Not used with sqlite3.
+        "PASSWORD": "",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
+    }
+}
+
+# Allowed development hosts
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "::1"]
